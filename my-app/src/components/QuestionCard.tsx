aefe2ecbd173301ad905c9f73ecd28d0c1036df1
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-07-30 00:58:10 +0900

    feat: :bug: QuestionCardの文章すべての行にアンダーラインがひかれるように修正

--- my-app/src/components/QuestionCard.tsx
+++ my-app/src/components/QuestionCard.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-//FIXME: 点線の長さがばらばらになる
-function splitIntoRectWidth(question?: string) {
-  const rectWidth = 200;
+import React, { useEffect, useRef, useState } from "react";
+
+function splitIntoRectWidth(question: string, rectWidth: number) {
   const result = [];
-  if (!question) return;
+  if (!question) return [];
   while (question.length > 0) {
     const fittedText = getFittedTextWithinWidth(question, rectWidth);
     result.push(fittedText);
@@ -15,11 +14,12 @@ function splitIntoRectWidth(question?: string) {
 
 function getFittedTextWithinWidth(text: string, rectWidth: number) {
   let chunk = "";
+  const ctx = document.createElement("canvas").getContext("2d");
+  if (!ctx) return;
+  ctx.font = "18px sans-jp";
+  
   for (let i = 0; i < text.length; i++) {
     chunk += text[i];
-    const ctx = document.createElement("canvas").getContext("2d");
-    if (!ctx) return;
-    ctx.font = "14px sans-serif";
     const chunkWidth = ctx.measureText(chunk).width;
     if (chunkWidth > rectWidth) {
       chunk = chunk.slice(0, -1);
@@ -27,22 +27,40 @@ function getFittedTextWithinWidth(text: string, rectWidth: number) {
     }
   }
   return chunk;
-
 }
-const QuestionCard = ({ question }: { question: string }) => {
+
+const QuestionCard = ({ question }: {question: string}) => {
+  const [rectWidth, setRectWidth] = useState(0);
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    if (cardRef.current) {
+      const width = (cardRef.current as HTMLElement)?.offsetWidth || 0;
+      setRectWidth(width);
+    }
+  }, []);
+
   const questionText = 'Q. ' + question;
-  const splitQuestion = splitIntoRectWidth(questionText);
+  const splitQuestion = splitIntoRectWidth(questionText, rectWidth);
 
   return (
-    <div className="p-6 shadow-md" 
-    style={{ 
-      backgroundImage: "url('/paper.jpg')", 
-      backgroundSize: "cover",
-    }}>
-    {splitQuestion?.map((text, index) => (
-      <span key={index} className="text-lg border-dashed border-b border-orange-900">{text}</span>
-    ))}
-    </div> 
+    <div
+      className="p-6 shadow-md"
+      style={{
+        backgroundImage: "url('/paper.jpg')",
+        backgroundSize: "cover",
+      }}
+    >
+      {splitQuestion.map((text, index) => (
+        <span
+          ref={cardRef}
+          key={index}
+          className="w-full block text-lg border-dashed border-b border-orange-900"
+        >
+          {text}
+        </span>
+      ))}
+    </div>
   );
 }
 
