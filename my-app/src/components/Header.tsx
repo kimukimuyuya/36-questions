4f9ea93b0fd4247e308004b7c8873daad7351dd9
kimukimuyuya <yuya150827@icloud.com>
2024-07-29 11:16:17 +0900

    ✨: 背景の変更、bgColorとして使用可能

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
+    <header className="bg-red-700 text-white p-4">
       <h1 className="text-2xl">36の質問アプリ</h1>
     </header>
   );
--- my-app/src/components/QuestionCard.tsx
+++ my-app/src/components/QuestionCard.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Image from "next/image";
 
 const QuestionCard = ({ question }: { question: string }) => {
   return (
-    <div className="bg-yellow-50 p-6 rounded-lg shadow-md border-2 border-yellow-200" 
-    style={{ fontFamily: "'Dancing Script', cursive", backgroundImage: "url('/paper.jpg')", backgroundSize: "cover" }}>
-      <p className="text-lg text-brown-700">{question}</p>
+    <div className="p-6 shadow-md" 
+    style={{ backgroundImage: "url('/paper.jpg')", backgroundSize: "cover" }}>
+      <p className="text-lg border-dashed border-b border-orange-900">
+        {question}
+      </p>
     </div> 
   );
 }
