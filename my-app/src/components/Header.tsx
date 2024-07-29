977207f2c5e64235e79e3e9079c0e841f45752c5
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-07-29 12:08:01 +0900

    ✨: アイコンを押すと、ホーム画面へ遷移

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="bg-red-700 text-white p-4">
-      <h1 className="text-2xl">36の質問アプリ</h1>
+      <h1 className="text-2xl">
+        <Link href="/">
+          36の質問
+        </Link>
+      </h1>
     </header>
   );
 }
