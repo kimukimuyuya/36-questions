6ac607569ecac9eab779919a4ed4a6c1faeaef4f
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-07-29 12:59:08 +0900

    🎨: fontをNoto sans JPに変更、next/fontを用いてフォント最適化

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Roboto } from "@next/font/google"
+import { Noto_Sans_JP } from "next/font/google";
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSansJP.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
