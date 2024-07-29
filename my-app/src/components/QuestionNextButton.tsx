c308d1bed5d117248afc9aabdf2bdac4276595b3
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-07-29 14:43:37 +0900

    ♻️: patternという表現がわかりにくいのでリファクタ

--- my-app/src/components/QuestionNextButton.tsx
+++ my-app/src/components/QuestionNextButton.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Link from "next/link";
 
-const QuestionChangeButton = ({ nextPatternId }: { nextPatternId: number }) => {
+const QuestionChangeButton = ({ nextQuestionLevel }: { nextQuestionLevel: number }) => {
   return (
-    <Link href={`/questions/${nextPatternId}`} className="bg-red-700 text-white py-2 px-4 rounded hover:bg-red-700 transition duration-300 ease-in-out">
+    <Link href={`/questions/${nextQuestionLevel}`} className="bg-red-700 text-white py-2 px-4 rounded hover:bg-red-700 transition duration-300 ease-in-out">
         次へ
     </Link>
   );
--- my-app/src/pages/questions/[question_level].tsx
+++ my-app/src/pages/questions/[question_level].tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { questionsPattern1, questionsPattern2, questionsPattern3 } from '../../data/questions';
+import { questionsLevel1, questionsLevel2, questionsLevel3 } from '../../data/questions';
 import QuestionCard from '../../components/QuestionCard';
 import QuestionChangeButton from '../../components/QuestionChangeButton';
 import QuestionNextButton from '../../components/QuestionNextButton';
@@ -12,51 +12,50 @@ const getRandomQuestion = (questions: string[]) => {
   return questions[Math.floor(Math.random() * questions.length)];
 };
 
-const PatternPage = () => {
+const QuestionPage = () => {
   const router = useRouter();
-  const pattern_id: string = router.query.pattern_id as string;
+  const questionLevel: string = router.query.question_level as string;
+  const nextQuestionLevel = Number(questionLevel) + 1;
+
   const getQuestions = (id: string) => {
     switch (id) {
       case '1':
-        return questionsPattern1;
+        return questionsLevel1;
       case '2':
-        return questionsPattern2;
+        return questionsLevel2;
       case '3':
-        return questionsPattern3;
+        return questionsLevel3;
       default:
-        return questionsPattern1;
+        return questionsLevel1;
     }
   };
 
-  const questions = getQuestions(pattern_id as string);
+  const questions = getQuestions(questionLevel as string);
   const [currentQuestion, setCurrentQuestion] = useState('');
   const [usedQuestions, setUsedQuestions] = useState<string[]>([]);
 
   useEffect(() => {
     setCurrentQuestion(getRandomQuestion(questions));
-  }, [pattern_id, questions]);
+  }, [questionLevel, questions]);
 
   const handleChangeQuestion = () => {
     setCurrentQuestion(getRandomQuestion(questions));
     setUsedQuestions([...usedQuestions, currentQuestion]);
-    console.log(usedQuestions);
   };
-
-  const nextPatternId = Number(pattern_id) + 1;
 
   return (
     <div>
       <Header />
       <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-bgColor">
-        <ProgressBar level={pattern_id} />
+        <ProgressBar level={questionLevel} />
         <QuestionCard question={currentQuestion} />
         <div className='flex items-center justify-around w-full mt-4'>
           <QuestionChangeButton onClick={handleChangeQuestion} />
-          <QuestionNextButton nextPatternId={nextPatternId} />
+          <QuestionNextButton nextQuestionLevel={nextQuestionLevel} />
         </div>
       </main>
     </div>
   );
 };
 
-export default PatternPage;
+export default QuestionPage;
