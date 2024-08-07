24f0b76e04e7796c03f6e19524ab717f8d95c1a7
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-08-07 18:15:17 +0900

    fix: :art: duration, delayを適切な値に設定＆封筒を開いた後に、カードが適切な位置に戻るように修正

--- my-app/src/components/Envelope.tsx
+++ my-app/src/components/Envelope.tsx
@@ -3,22 +3,27 @@ import QuestionCard from './QuestionCard';
 
 const Envelope = ({ onClick }: { onClick: () => void }) => {
   const [opened, setOpened] = useState(false);
+  const [reset, setReset] = useState(false);
 
   const handleClick = () => {
     setOpened(!opened);
+    setTimeout(() => {
+      setReset(true);
+    }, 1000);
   };
 
   return (
     <div
-      className={`relative w-[300px] sm:w-[350px] transition-all duration-700 aspect-video flex items-center justify-center cursor-pointer`}
+      className={`shadow-md relative w-[300px] sm:w-[350px] transition-all duration-700 aspect-video flex items-center justify-center cursor-pointer`}
       onClick={handleClick}
     >
       <div
-        className={`transition-all flex flex-col items-center justify-start duration-300 ${
-          opened ? 'duration-1000 -translate-y-16' : ''
-        } bg-white w-full h-full absolute`}
+        className={`transition-all delay-500 w-full h-full absolute
+        ${ opened ? 'duration-1000 -translate-y-16' : ''}
+        ${ reset ? 'duration-0 translate-y-0' : ''}
+        `}
       >
-        <QuestionCard question='Q. What is the capital of Japan?' />
+        <QuestionCard question='あなたの好きなポケモンはなんですか？' />
       </div>
       <button
         className={`seal bg-rose-500 text-red-800 w-10 aspect-square rounded-full z-40 text-[10px] flex items-center justify-center font-semibold [clip-path:polygon(50%_0%,_80%_10%,_100%_35%,_100%_70%,_80%_90%,_50%_100%,_20%_90%,_0%_70%,_0%_35%,_20%_10%)]${
@@ -28,20 +33,24 @@ const Envelope = ({ onClick }: { onClick: () => void }) => {
         SMKY
       </button>
       <div
-        className={`tp transition-all duration-1000 absolute [clip-path:polygon(50%_50%,_100%_0,_0_0)] w-full h-full bg-cover bg-[url('/pink_paper.jpg')] ${
+        className={`tp transition-all duration-1000 absolute w-full h-full bg-cover bg-[url('/pink_paper.jpg')] ${
           opened
-            ? 'duration-0 [clip-path:polygon(50%_0%,_100%_0,_0_0)] opacity-0'
-            : ''
+            ? 'duration-0 opacity-0 [clip-path:polygon(50%_0%,_100%_0,_0_0)]'
+            : '[clip-path:polygon(50%_50%,_100%_0,_0_0)]'
         }`}
+      >
+      </div>
+      <div
+        className={`lft transition-all duration-700 absolute w-full h-full [clip-path:polygon(50%_50%,_0_0,_0_100%)] bg-cover bg-[url('/pink_paper.jpg')] 
+          ${opened ? 'opacity-0 delay-1000' : ''}`}
       ></div>
       <div
-        className={`lft transition-all duration-700 absolute w-full h-full [clip-path:polygon(50%_50%,_0_0,_0_100%)] bg-cover bg-[url('/pink_paper.jpg')] `}
+        className={`rgt transition-all duration-700 absolute w-full h-full [clip-path:polygon(50%_50%,_100%_0,_100%_100%)] bg-cover bg-[url('/pink_paper.jpg')]
+          ${opened ? 'opacity-0 delay-1000' : ''}`}
       ></div>
       <div
-        className={`rgt transition-all duration-700 absolute w-full h-full [clip-path:polygon(50%_50%,_100%_0,_100%_100%)] bg-cover bg-[url('/pink_paper.jpg')]`}
-      ></div>
-      <div
-        className={`btm transition-all duration-700 absolute w-full h-full bg-[#ecdeb8] [clip-path:polygon(50%_50%,_100%_100%,_0_100%)] bg-cover bg-[url('/pink_paper.jpg')]`}
+        className={`btm transition-all duration-700 absolute w-full h-full bg-[#ecdeb8] [clip-path:polygon(50%_50%,_100%_100%,_0_100%)] bg-cover bg-[url('/pink_paper.jpg')]
+          ${opened ? 'opacity-0 delay-1000' : ''}`}
       ></div>
     </div>
   );
