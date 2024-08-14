d2c77024fb0bf71ce349be0071f7d8eb728bb9d1
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-08-15 08:00:56 +0900

    fix: :fire: コピーライトは不要そうなので削除

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -25,9 +25,9 @@ const Home = () => {
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      {/* <footer className="flex items-center justify-center w-full h-24 border-t">
         <p className="text-gray-500">© 2024 36の質問. All rights reserved.</p>
-      </footer>
+      </footer> */}
     </div>
   );
 };
