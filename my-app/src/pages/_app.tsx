7ae857da3d21aec68142866e198f6671b949dc16
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-08-16 20:25:27 +0900

    fix: :fire: googlefontがうまく読み込めず、importでエラーが起きていたため修正

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Noto_Sans_JP } from "next/font/google";
+// import { Noto_Sans_JP } from "next/font/google";
 
-const notoSansJP = Noto_Sans_JP({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
+// const notoSansJP = Noto_Sans_JP({
+//   subsets: ["latin"],
+//   weight: ["400", "700"],
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={notoSansJP.className}>
-      <Component {...pageProps} />
-    </main>
+    // <main className={notoSansJP.className}>
+    //   <Component {...pageProps} />
+    // </main>
+    <Component {...pageProps} />
   )
 }
