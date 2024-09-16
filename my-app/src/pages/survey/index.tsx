3c3d81cbe9cdd1f23b39f21ba5ace63b492400d5
kimukimuyuya <yuya150827@icloud.com>
2024-09-16 17:43:40 +0900

    feat: :recycle: uiを整えて最低限動く状態に

--- my-app/src/pages/survey/index.tsx
+++ my-app/src/pages/survey/index.tsx
@@ -1,39 +1,71 @@
-import { FormEvent, useState} from 'react';
+import { useState } from 'react';
 import Header from '../../components/Header';
 import router from 'next/router';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  content: z.string(),
+});
 
 const Survey = () => {
   const [content, setContent] = useState<string>('');
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();  // デフォルトのフォーム送信を防ぐ
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { content: '' },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     const response = await fetch('/api/survey', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ content }),
+      body: JSON.stringify(values),
     });
 
     if (response.ok) {
       console.log('Survey result created');
       router.push('/thanks');  // 成功した場合に遷移
     }
-  };
+  }
 
   return (
     <div className='min-h-screen bg-bgColor'>
       <Header />
-      <h1>Survey</h1>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>最後に、貴方が思う「こんな話題なら関係性が深まる」を教えていただきたいです</FormLabel>
+                <FormControl>
+                  <Input placeholder="ex. 人生で一番しんどかった出来事は?" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">送信する</Button>
+        </form>
+      </Form>
     </div>
-  );
+  )
 }
 
 export default Survey;
