646da95675f470470d0114f8a909957245ff11cc
kimukimuyuya <yuya150827@icloud.com>
2024-09-16 15:54:29 +0900

    feat: :sparkles: thanksページの作成

---
+++ my-app/src/pages/thanks/index.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header';
+
+const Thanks = () => {
+  return (
+    <div className='min-h-screen bg-bgColor'>
+      <Header />
+      <h1>thanks!</h1>
+    </div>
+  );
+}
+
+export default Thanks;
