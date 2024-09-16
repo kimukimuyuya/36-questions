bc40fe52d565329a14a6ece75de20a2707588371
kimukimuyuya <yuya150827@icloud.com>
2024-09-16 16:34:30 +0900

    fix: :bug: google fontの適用

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,17 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-// import { Noto_Sans_JP } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 
-// const notoSansJP = Noto_Sans_JP({
-//   subsets: ["latin"],
-//   weight: ["400", "700"],
-// });
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <main className={notoSansJP.className}>
-    //   <Component {...pageProps} />
-    // </main>
-    <Component {...pageProps} />
+    <main className={notoSansJP.className}>
+      <Component {...pageProps} />
+    </main>
   )
 }
