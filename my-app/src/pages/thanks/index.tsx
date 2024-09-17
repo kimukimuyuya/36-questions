8664ce4e19393297317c58aa39b9677298f7421a
kimukimuyuya <yuya150827@icloud.com>
2024-09-17 17:50:24 +0900

    feat: :art: 最低限のUIを実装

--- my-app/src/pages/thanks/index.tsx
+++ my-app/src/pages/thanks/index.tsx
@@ -4,11 +4,21 @@ const Thanks = () => {
   return (
     <div className='min-h-screen bg-bgColor'>
       <Header />
-      <main className='flex flex-col items-center justify-center py-2'>
-        <p>ご回答いただきありがとうございます。</p>
-        <p>素敵な時間を過ごすことはできましたでしょうか?</p>
-        <p>相手の新しい一面を見ることはできましたでしょうか?</p>
-        <p>もし、関係性を深めるお手伝いができていましたら光栄です。</p>
+      <main className='flex flex-col items-center justify-center py-10 text-baseColor'>
+        <div className='text-center space-y-4'>
+          <p className='text-xl font-semibold'>
+            ご回答いただきありがとうございます。
+          </p>
+          <p className='text-md'>
+            素敵な時間を過ごすことはできたでしょうか?
+          </p>
+          <p className='text-md'>
+            相手の新しい一面を見ることはできたでしょうか?
+          </p>
+          <p className='text-md'>
+            もし、関係性を深めるお手伝いができていましたら光栄です。
+          </p>
+        </div>
       </main>
     </div>
   );
