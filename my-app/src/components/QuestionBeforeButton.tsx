62fa41552ba88642c8d864fd35b70fb55938d6d1
kimukimuyuya <yuya150827@icloud.com>
2024-09-17 17:17:28 +0900

    feat: :art: 色の指定方法を変更

--- my-app/src/components/QuestionBeforeButton.tsx
+++ my-app/src/components/QuestionBeforeButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const QuestionChangeButton = ({ beforeQuestionLevel }: { beforeQuestionLevel: number }) => {
 
   return (
-    <Link href={`/questions/${beforeQuestionLevel}`} className="  bg-red-700 text-white py-2 px-4 rounded hover:bg-red-700 transition duration-300 ease-in-out">
+    <Link href={`/questions/${beforeQuestionLevel}`} className="  bg-baseColor text-white py-2 px-4 rounded hover:bg-baseColor transition duration-300 ease-in-out">
         前へ
     </Link>
   );
--- my-app/src/components/QuestionNextButton.tsx
+++ my-app/src/components/QuestionNextButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const QuestionChangeButton = ({ nextQuestionLevel }: { nextQuestionLevel: number }) => {
   return (
-    <Link href={`/questions/${nextQuestionLevel}`} className="bg-red-700 text-white py-2 px-4 rounded hover:bg-red-700 transition duration-300 ease-in-out">
+    <Link href={`/questions/${nextQuestionLevel}`} className="bg-baseColor text-white py-2 px-4 rounded hover:bg-baseColor transition duration-300 ease-in-out">
         次へ
     </Link>
   );
