42207c571698d85900af3942ffb4c21100c5a1e1
kimukimuyuya <yuya150827@icloud.com>
2024-09-18 11:44:22 +0900

    feat: :art: LP作業途中だがいったんコミット

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,49 +1,78 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import router from 'next/router';
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-bgColor">
+    <div className="flex flex-col items-center justify-center min-h-screen py-4 bg-bgColor">
       <Head>
         <title>36の質問 - ランディングページ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-4 text-center my-32">
-        <div className="flex items-center justify-center">
-          <Image
-            src="/bride.png"
-            alt="新婦"
-            width={200}
-            height={200}
-          />
-          <Image
-            src="/bridegroom.png"
-            alt="新郎"
-            width={200}
-            height={200}
-          />
+      <main className="flex flex-col items-center justify-center flex-1 mx-8 text-center mt-20">
+        <div className='mb-12 bg-[url("/couple.png")] bg-cover bg-center w-full h-96 flex flex-col items-center justify-center'>
+          <p>関係性を深める、話題提供アプリ</p>
+          <h1 className="text-4xl font-bold mt-2 text-baseColor">36の質問</h1>
         </div>
-        <Image 
-          src="/book.png"
-          alt="本"
-          width={100}
-          height={100}
-        />
-        <h1 className="text-5xl font-bold mt-4">36の質問</h1>
-        <p className="mt-3 text-lg">
-          大切な人と試してみよう
-        </p>
-
-        <div className="mt-6">
-          <Link
-            href="/questions/1"
-            className="px-8 py-3 text-lg font-medium text-white bg-baseColor rounded-md hover:bg-baseColor"
-          >
+        <div className=''>
+          <p className="text-sm">
+            あなたは気になるあの子と、
+          </p>
+          <p className="mt-3 text-sm">
+            昔からの友人と、
+          </p>
+          <p className="mt-3 text-sm">
+            長年連れ添ったパートナーと、
+          </p>
+          <p className="mt-3 text-sm">
+            ○○できていますか?
+          </p>
+          <p className="mt-3 text-sm">
+            深い対話ができる関係性の、
+          </p>
+          <p className="mt-3 text-sm">
+            きっかけ作りができれば幸いです。
+          </p>
+          </div>
+          <div className="flex items-center justify-center mt-8">
+            <Image
+              src="/bride.png"
+              alt="新婦"
+              width={150}
+              height={150}
+            />
+            <Image
+              src="/bridegroom.png"
+              alt="新郎"
+              width={150}
+              height={150}
+            />
+        </div>
+        <Button className='p-5 text-md my-8 bg-baseColor hover:bg-baseColor' onClick={() => router.push('/questions/1')}>
             始める
-          </Link>
+        </Button>
+        <div className='mt-12'>
+          <p className='text-sm'>
+            それでは、互いの心の中を覗く
+          </p>
+          <p className='text-sm mt-3'>
+            素敵な時間を。
+          </p>
+          <div className='mt-6 flex justify-center'>
+            <Image 
+              src="/book.png"
+              alt="本"
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
+        <Button className='p-5 text-md my-8 bg-baseColor hover:bg-baseColor' onClick={() => router.push('/questions/1')}>
+          始める
+        </Button>
       </main>
 
       {/* <footer className="flex items-center justify-center w-full h-24 border-t">
