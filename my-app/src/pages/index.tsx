35e4fa1e7fe5c465d6473f2db35fa8ec63b4f9ad
kimukimuyuya <yuya150827@icloud.com>
2024-09-18 10:53:38 +0900

    feat: :art: 使用する画像を表示

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Home = () => {
   return (
@@ -9,7 +10,27 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-4 text-center">
+      <main className="flex flex-col items-center justify-center flex-1 px-4 text-center my-32">
+        <div className="flex items-center justify-center">
+          <Image
+            src="/bride.png"
+            alt="新婦"
+            width={200}
+            height={200}
+          />
+          <Image
+            src="/bridegroom.png"
+            alt="新郎"
+            width={200}
+            height={200}
+          />
+        </div>
+        <Image 
+          src="/book.png"
+          alt="本"
+          width={100}
+          height={100}
+        />
         <h1 className="text-5xl font-bold mt-4">36の質問</h1>
         <p className="mt-3 text-lg">
           大切な人と試してみよう
