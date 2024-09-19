3d2fc70119ad5d32b025f94a59cf601c3600c738
kimukimuyuya <yuya150827@icloud.com>
2024-09-19 20:41:38 +0900

    feat: :art: dialogのUI完了

--- my-app/src/components/ CourseSelectionDialog.tsx
+++ my-app/src/components/ CourseSelectionDialog.tsx
@@ -1,5 +1,3 @@
-import { CopyIcon } from "@radix-ui/react-icons"
-
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -11,10 +9,19 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import React, { useState } from "react"
 
 function DialogCloseButton() {
+  const [selectedValue, setSelectedValue] = useState('5');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log(selectedValue);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -22,30 +29,58 @@ function DialogCloseButton() {
           コースを選択
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-[90%] sm:max-w-md mx-auto rounded-lg">
+      <DialogContent className="max-w-[90%] sm:max-w-md mx-auto rounded-lg bg-bgColor">
         <DialogHeader>
-          <DialogTitle>コースを選択してください</DialogTitle>
+          <DialogTitle className="text-center text-xl">コースを選択してください</DialogTitle>
         </DialogHeader>
-        {/* <div className="flex items-center space-x-2"> */}
-        <fieldset className="radio-3">
-          <label>
-            <input type="radio" name="radio-3" />
-            radio1
-          </label>
-          <label>
-            <input type="radio" name="radio-3" />
-            radio2
-          </label>
-          <label>
-            <input type="radio" name="radio-3" />
-            radio3
-          </label>
-        </fieldset>
-        {/* </div> */}
-        <DialogFooter className="sm:justify-start">
+        <div className="flex items-center justify-center my-4 text-md">
+          <fieldset className="radio-3">
+            <label>
+              <input
+                type="radio"
+                name="radio-3"
+                value="5"
+                checked={selectedValue === '5'}
+                onChange={handleChange}
+              />
+              30分(5問)
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="radio-3"
+                value="10"
+                checked={selectedValue === '10'}
+                onChange={handleChange}
+              />
+              1時間(10問)
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="radio-3"
+                value="18"
+                checked={selectedValue === '18'}
+                onChange={handleChange}
+              />
+              2時間(18問)
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="radio-3"
+                value="36"
+                checked={selectedValue === '36'}
+                onChange={handleChange}
+              />
+              4時間(36問)
+            </label>
+          </fieldset>
+        </div>
+        <DialogFooter className="flex justify-center items-center">
           <DialogClose asChild>
-            <Button type="button" variant="secondary">
-              Close
+            <Button type="button" className=" w-3/4 text-sm bg-baseColor hover:bg-baseColor" onClick={handleButtonClick}>
+              始める
             </Button>
           </DialogClose>
         </DialogFooter>
