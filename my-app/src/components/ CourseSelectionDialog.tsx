b4bf8cd7275ae50a07988668805a4fdd6ee3dd60
kimukimuyuya <yuya150827@icloud.com>
2024-09-19 19:23:07 +0900

    feat: :sparkles: dialogのサンプルを持ってくる

---
+++ my-app/src/components/ CourseSelectionDialog.tsx
@@ -0,0 +1,58 @@
+import { CopyIcon } from "@radix-ui/react-icons"
+
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+function DialogCloseButton() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className='p-5 text-md bg-baseColor hover:bg-baseColor'>
+          コースを選択
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[90%] sm:max-w-md mx-auto rounded-lg">
+        <DialogHeader>
+          <DialogTitle>コースを選択してください</DialogTitle>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input
+              id="link"
+              defaultValue="https://ui.shadcn.com/docs/installation"
+              readOnly
+            />
+          </div>
+          <Button type="submit" size="sm" className="px-3">
+            <span className="sr-only">Copy</span>
+            <CopyIcon className="h-4 w-4" />
+          </Button>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default DialogCloseButton
+
