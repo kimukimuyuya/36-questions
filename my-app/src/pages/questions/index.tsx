83b11f378431b0d53db5c0e9fe7346a3381a19a5
kimukimuyuya <yuya150827@icloud.com>
2024-09-21 21:59:04 +0900

    feat: :sparkles: 質問表示をいったんcommit

---
+++ my-app/src/pages/questions/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { questionsLevel1, questionsLevel2, questionsLevel3 } from '../../data/questions';
+import QuestionCard from '../../components/QuestionCard';
+import QuestionChangeButton from '../../components/QuestionChangeButton';
+import QuestionNextButton from '../../components/QuestionNextButton';
+import QuestionBeforeButton from '../../components/QuestionBeforeButton';
+import Envelope from '@/components/Envelope';
+import StepBar from '../../components/StepBar';
+import Header from '../../components/Header';
+import { useRouter } from 'next/router';
+import useQuestionsStore from '@/store/questionsStore';
+
+const QuestionPage = () => {
+  const router = useRouter();
+  const questions = useQuestionsStore(state => state.questions);
+  const [currentQuestion, setCurrentQuestion] = useState('');
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  console.log(questions);
+
+  useEffect(() => {
+    if (questions.length === 0) {
+      // データがない場合はリダイレクトするなどの処理
+      router.push('/');
+    }
+  }, [questions, router]);
+
+  const nextQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  if (questions.length === 0) return <div>Loading...</div>;
+
+  return (
+    <div className='min-h-screen bg-bgColor'>
+      <Header />
+      <div>
+        <h1>質問</h1>
+        <p>{questions[currentQuestionIndex]?.content}</p>
+        <p>{currentQuestion}</p>
+        <button onClick={nextQuestion}>次へ</button>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionPage;
