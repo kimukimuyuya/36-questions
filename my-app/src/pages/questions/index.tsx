15e06d25fc8da758bab1684129edc0e0d231392e
kimukimuyuya <yuya150827@icloud.com>
2024-09-22 19:14:10 +0900

    feat: :art: 質問が終了したときに、終わったことを知らせるメッセージ表示

--- my-app/src/pages/questions/index.tsx
+++ my-app/src/pages/questions/index.tsx
@@ -18,6 +18,7 @@ const useCurrentQuestion = (questions: Question[]) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [currentQuestionLevel, setCurrentQuestionLevel] = useState<number>(1);
   const [showLevelMessage, setShowLevelMessage] = useState<boolean>(false);
+  const [showEndAnimation, setShowEndAnimation] = useState<boolean>(false);
 
   useEffect(() => {
     if (questions.length > 0) {
@@ -35,42 +36,57 @@ const useCurrentQuestion = (questions: Question[]) => {
       }
       setCurrentQuestionIndex(nextIndex);
     } else {
-      router.push('/survey');
+      setShowEndAnimation(true);
+      setTimeout(() => {
+        router.push('/survey');
+      }, 2000);
     }
   };
 
-  return { currentQuestionContent, currentQuestionLevel, showLevelMessage, nextQuestion };
+  return { currentQuestionContent, currentQuestionLevel, showLevelMessage, showEndAnimation, nextQuestion };
 };
 
 const QuestionPage = () => {
   const questions = useQuestionsStore(state => state.questions);
-  const { currentQuestionContent, currentQuestionLevel, showLevelMessage, nextQuestion } = useCurrentQuestion(questions);
+  const { currentQuestionContent, currentQuestionLevel, showLevelMessage, showEndAnimation, nextQuestion } = useCurrentQuestion(questions);
 
   return (
     <div className='min-h-screen bg-bgColor'>
       <Header />
-      <div className='mb-20 sm:mb-24'>
-        <StepBar QuestionLevel={currentQuestionLevel} />
-      </div>
-      <main className="flex flex-col items-center p-4">
-        {showLevelMessage ? (
-          <div className={`text-3xl font-bold my-10 animate__animated animate__fadeIn ${currentQuestionLevel === 2 ? 'text-subColor' : 'text-baseColor'}`}>
-            レベル{currentQuestionLevel}
+      {showEndAnimation ? (
+        <div className="flex flex-col items-center justify-center animate__animated animate__fadeIn">
+          <div className='text-center mx-8 text-baseColor mt-40 text-md'>
+            <p className="mb-8">質問は以上となります。</p>
+            <p className='mb-3'>最後にアンケートに回答いただけますと</p>
+            <p>幸いです。</p>
           </div>
-        ) : (
-          <div className='relative md:w-4/6 w-full flex justify-center'>
-            <QuestionCard question={currentQuestionContent} />
-            <div className='absolute top-40 flex items-center justify-around w-full mt-12'>
-            <Button
-              onClick={nextQuestion}
-              className='bg-baseColor hover:bg-baseColor'
-            >
-              次の問題
-            </Button>
-            </div>
+        </div>
+      ) : (
+        <>
+          <div className='mb-20 sm:mb-24'>
+            <StepBar QuestionLevel={currentQuestionLevel} />
           </div>
-        )}
-      </main>
+          <main className="flex flex-col items-center p-4">
+            {showLevelMessage ? (
+              <div className={`text-3xl font-bold my-10 animate__animated animate__fadeIn ${currentQuestionLevel === 2 ? 'text-subColor' : 'text-baseColor'}`}>
+                レベル{currentQuestionLevel}
+              </div>
+            ) : (
+              <div className='relative md:w-4/6 w-full flex justify-center'>
+                <QuestionCard question={currentQuestionContent} />
+                <div className='absolute top-40 flex items-center justify-around w-full mt-12'>
+                  <Button
+                    onClick={nextQuestion}
+                    className='bg-baseColor hover:bg-baseColor'
+                  >
+                    次の問題
+                  </Button>
+                </div>
+              </div>
+            )}
+          </main>
+        </>
+      )}
     </div>
   );
 };
