eb80baca61377386feb8e4abe5e1d0ffa0910600
kimukimuyuya <yuya150827@icloud.com>
2024-09-22 13:48:16 +0900

    feat: :art: stepbarのカラーを調整

--- my-app/src/components/StepBar.tsx
+++ my-app/src/components/StepBar.tsx
@@ -3,20 +3,20 @@ import React from 'react';
 const StepBar = ({ QuestionLevel }: { QuestionLevel: number }) => {
   const currentLevel = Number(QuestionLevel);
   const levels = [
-    { number: 1, text: 'level1', color: 'bg-red-500' },
-    { number: 2, text: 'level2', color: 'bg-red-600' },
-    { number: 3, text: 'level3', color: 'bg-red-700' },
+    { number: 1, text: 'Lv.1', color: 'secondaryColor' },
+    { number: 2, text: 'Lv.2', color: 'subColor' },
+    { number: 3, text: 'Lv.3', color: 'baseColor' },
   ];
   // TODO: コンポーネントの分割
   return (
-    <div className="text-white text-base mt-10 sm:mt-20 uppercase">
+    <div className="text-white text-base mt-10 sm:mt-20">
       <ul className="flex justify-center space-x-10 sm:space-x-20">
         {levels.map((level, index) => (
           <li key={index} className="relative flex flex-col items-center">
             <div className="relative flex flex-col items-center">
               {/* 円 */}
               <div
-                className={`number ${currentLevel >= level.number ? 'active bg-baseColor text-white' : 'bg-white text-gray-500'} transition 
+                className={`number ${currentLevel >= level.number ? `active bg-${level.color} text-white` : 'bg-white text-gray-500'} transition 
                 duration-500 w-12 h-12 sm:w-16 sm:h-16 rounded-full flex items-center justify-center mb-1 sm:mb-2 font-bold z-10`}
                 style={{
                   transitionDelay: `${index * 0.2}s`
@@ -39,7 +39,9 @@ const StepBar = ({ QuestionLevel }: { QuestionLevel: number }) => {
               )}
               
             </div>
-            <div className="text text-base sm:text-lg">{level.text}</div>
+            <div className={`text text-base sm:text-lg  ${currentLevel >= level.number ? `active text-${level.color}` : 'text-gray-500'}`}>
+              {level.text}
+            </div>
           </li>
         ))}
       </ul>
