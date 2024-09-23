17c6ab8c3ae2817d9c330872123450af0440ba55
kimukimuyuya <yuya150827@icloud.com>
2024-09-23 09:04:40 +0900

    feat: :sparkles: サンクスページにHeadを追加

--- my-app/src/pages/thanks/index.tsx
+++ my-app/src/pages/thanks/index.tsx
@@ -3,10 +3,19 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import router from 'next/router';
 import 'animate.css'
+import { SeoHead } from '@/components/SeoHead';
 
 const Thanks = () => {
   return (
     <div className='min-h-screen bg-bgColor'>
+      <SeoHead
+        title={"36の質問"}
+        titleTemplate={"サンクスページ"}
+        description={"アンケートに回答していただいた後に表示するサンクスページです"}
+        keyWords={"36の質問, 関係性, コミュニケーション, パートナー, 友人, 恋人, カップル, 対話, 話題, 提供"}
+        ogType={"website"}
+        imgUrl={'/next.svg'}
+      />
       <Header />
       <main className='flex flex-col items-center pt-12 justify-center animate__animated animate__fadeIn'>
         <div className='text-center space-y-8 mx-8 mb-12 text-baseColor'>
